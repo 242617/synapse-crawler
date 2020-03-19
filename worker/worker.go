4a29056f8f7c23ab2862bfb060c8ba0470d8b723
d.kapsamun <242617@gmail.com>
2020-03-19 13:24:13 +0300

    Add gRPC ping

--- worker/worker.go
+++ worker/worker.go
@@ -1,11 +1,14 @@
 package worker
 
 import (
+	"context"
 	"time"
 
-	"github.com/getsentry/sentry-go"
+	"google.golang.org/grpc"
 
-	"github.com/242617/synapse-crawler/jobs/ping"
+	"github.com/242617/synapse-core/api"
+
+	"github.com/242617/synapse-crawler/config"
 	"github.com/242617/synapse-crawler/log"
 )
 
@@ -14,27 +17,40 @@ var logger log.Logger
 func Init(base log.Logger) error {
 	logger = base
 
+	// Check core online
 	go func() {
 		for {
-
-			job := ping.NewJob()
-			_, err := job.Do()
+			err := ping()
 			if err != nil {
-				logger.Warn().
+				logger.Error().
 					Err(err).
-					Msg("cannot ping core")
-
-				sentry.CaptureException(err)
-				defer sentry.Flush(5 * time.Second)
-
+					Msg("ping error")
 				time.Sleep(10 * time.Second)
 				continue
 			}
-
 			time.Sleep(5 * time.Second)
-
 		}
 	}()
+
+	return nil
+}
+
+func ping() error {
+
+	conn, err := grpc.Dial(config.Cfg.Core.Address, grpc.WithInsecure())
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	client := api.NewSystemClient(conn)
+	_, err = client.Info(ctx, &api.Void{})
+	if err != nil {
+		return err
+	}
 
 	return nil
 
